a4ba76ea2c875f1c23d3201f983459cd6a9709f4
Chenxiong Qi <qcxhome@gmail.com>
2021-02-05 19:19:10 +0800

    hello name
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -9,6 +10,19 @@ def index():
         <head><title>My Cloud</title></head>
         <body>
             <h1>Introduction</h1>
+            <p>A simple Web application.</p>
+        </body>
+     </html>
+    """
+
+
+@app.route('/<username>')
+def hello(username):
+    return f"""<html>
+        <head><title>My Cloud</title></head>
+        <body>
+            <h1>Introduction</h1>
+            <h2>Hello {escape(username)}</h2>
             <p>A simple Web application.</p>
         </body>
      </html>
